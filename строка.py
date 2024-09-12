f42f1f16f48cd0ea236b8ed8a3a85e9a2d3827e2
sendiVV <dankevich.viktoria.1kdd12@gmail.com>
2024-09-12 09:11:27 +0300

    Add files via upload

---
+++ строка.py
@@ -0,0 +1,4 @@
+namberOfTasks = "2"
+number_of_hours = "1"
+course = " python"
+print('курс;', course , 'всего задач;' , namberOfTasks , 'затрачено часов' , number_of_hours , 'среднеее время' , 1/2 )
